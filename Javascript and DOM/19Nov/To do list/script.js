23c02f733a69b525d922b065d65e5995cf9c87ea
pegais <snehal9140@gmail.com>
2023-11-19 13:56:08 +0530

    TODO list app

---
+++ Javascript and DOM/19Nov/To do list/script.js
@@ -0,0 +1,11 @@
+// JavaScript Logic:
+
+// Implement the JavaScript logic to add, delete, and mark tasks as completed.
+// Write a function to add a new task to the list when the user clicks the "Add Task" button.
+// Write a function to delete a task when the user clicks the "Delete" button next to the task.
+// Write a function to mark a task as completed when the user checks the checkbox next to the task.
+// User Interaction:
+
+// Allow users to input tasks into the input field.
+// Display tasks in an organized manner with options to mark them as completed or delete them.
+// Provide feedback to users when tasks are added, deleted, or marked as completed.
