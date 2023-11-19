abd40fbe226c9b0b99f9e3fd4b0edf8c572fba37
pegais <snehal9140@gmail.com>
2023-11-19 15:56:09 +0530

    TO DO list

--- Javascript and DOM/19Nov/To do list/script.js
+++ Javascript and DOM/19Nov/To do list/script.js
@@ -9,3 +9,52 @@
 // Allow users to input tasks into the input field.
 // Display tasks in an organized manner with options to mark them as completed or delete them.
 // Provide feedback to users when tasks are added, deleted, or marked as completed.
+const button =document.querySelector('button');
+
+button.addEventListener('click',addtask)
+function addtask(){
+    // select and get value of input box
+    //select the btn and add a event click
+    const inputBox =document.getElementById('taskInput');
+    const taskUL =document.getElementById('taskList'); //Ul tag
+    console.log(inputBox.value,"this is input value");
+
+    if(inputBox.value !==""){
+        //we will create li;
+        //we will append this to UL
+        
+        const li =document.createElement('li');
+        // li.innerText=inputBox.value;
+        
+        li.innerHTML=`
+        <span>${inputBox.value}</span>
+            <button onclick=deletetask(this)>Delete</button>
+            <input type='checkbox' onchange=completetask(this)>
+        `
+        taskUL.appendChild(li);
+        inputBox.value=''
+    }
+}
+
+function deletetask(button){
+    //this : special keyword in js
+    // this points to the object in which it is called;
+//    parentElement, remove();
+
+    const li= button.parentElement;
+    console.log(li);
+    li.remove();
+}
+
+function completetask(input){
+    // console.log(input,"this is what this keyword is pointing");
+    const span =input.parentElement.querySelector('span');
+    console.log(span,"span tag value");
+   if(input.checked){
+    // task should be completed marked.
+    span.style.textDecoration="line-through";
+   }else{
+    span.style.textDecoration="none";
+   }
+
+}
